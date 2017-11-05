2befb38ae56617e2888846c03f22ed27cd3d1513
Kaotisk Hund <kaotiskhund@gmail.com>
2017-11-05 15:06:11 +0200

    Added comments

--- bin/append_whitelist.sh
+++ bin/append_whitelist.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #
+# This script does the following:
+
 # Append whitelist to opentracker's whitelist
+cat whitelist >> /var/opentracker/whitelist
+
 # Empty local whitelist
 cat /dev/null > whitelist
 
-cat whitelist >> /var/opentracker/whitelist
+# TODO: As it should be removing the list only
+# if the append to opentracker's one is successful.
+
+# Consider running it as root or chmod 777 your opentracker's
+# whitelist so you don't lose local whitelist infoHashes in case
+# of permission errors.
